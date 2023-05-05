3bf4fc4f43f6054412505621ce7c07a9d5354c89
Artemiy Bal <artemiy.bal@helmes.com>
2023-05-05 11:48:55 +0300

    creating mobile layout

--- src/components/pages/DrivingDistancePage/subcomponents/DescriptionSection/DescriptionSection.tsx
+++ src/components/pages/DrivingDistancePage/subcomponents/DescriptionSection/DescriptionSection.tsx
@@ -3,7 +3,7 @@ import { CarMarkerIcon, CityIcon } from "~/src/assets";
 export const DescriptionSection = () => (
   <section className="relative mb-3 w-full bg-gradientLeft lg:mb-7">
     <div className="container pb-9 pt-7 lg:pb-[5.4rem] lg:pt-[4.8rem]">
-      <h2 className="relative z-10 mb-9 text-center text-[2.5rem] font-bold leading-[2.7rem] tracking-wider text-white lg:mb-[5.4rem] lg:text-[4.5rem] lg:leading-[5rem] lg:tracking-wide">
+      <h2 className="relative z-10 mb-[7.4rem] text-center text-[2.5rem] font-bold leading-[2.7rem] tracking-wider text-white lg:mb-[5.4rem] lg:text-[4.5rem] lg:leading-[5rem] lg:tracking-wide">
         Distance from Beijing to Beppu
       </h2>
       <div className="relative mx-auto w-[93%] rounded-md bg-white px-2 pb-7 pt-4 lg:w-[63%] lg:px-6 lg:pb-5 lg:pt-7">
@@ -22,7 +22,7 @@ export const DescriptionSection = () => (
           </div>
         </div>
         <div className="dashed-border h-[1.5px] w-full" />
-        <div className="mt-14 text-center lg:mt-[3.2rem]">
+        <div className="mt-12 text-center lg:mt-[3.2rem]">
           <h3 className="font-bold lg:text-xl">
             The driving distance between San Antonio and Playa del Carmen
           </h3>
@@ -31,7 +31,7 @@ export const DescriptionSection = () => (
             hours.
           </h4>
         </div>
-        <CarMarkerIcon className="absolute left-1/2 top-14 w-[4.2rem] -translate-x-1/2 text-redBg" />
+        <CarMarkerIcon className="absolute left-1/2 top-10 lg:top-14 w-[4.2rem] -translate-x-1/2 text-redBg" />
       </div>
     </div>
   </section>
