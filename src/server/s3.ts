bffe0dd9945890b36618aa12c363a9faa530e8e3
Vailery <leraliabik8@gmail.com>
2023-05-05 11:10:38 +0300

    refactor: change name of keys

--- src/server/s3.ts
+++ src/server/s3.ts
@@ -6,16 +6,16 @@ const getCredentials = ():
   | AwsCredentialIdentity
   | Provider<AwsCredentialIdentity> => {
   if (
-    process.env.AWS_ACCESS_KEY == null &&
-    process.env.AWS_SECRET_KEY == null
+    process.env.AWS_ACCESS_KEY_ID == null &&
+    process.env.AWS_SECRET_ACCESS_KEY == null
   ) {
     return fromSSO({
       profile: process.env.AWS_SSO_PROFILE,
     });
   } else {
     return {
-      accessKeyId: process.env.AWS_ACCESS_KEY ?? "",
-      secretAccessKey: process.env.AWS_SECRET_KEY ?? "",
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
     };
   }
 };
